a90e996bdb328e71bd4ab2ceb7d8454095b99e22
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2025-04-10 11:11:27 +0900

    feat: initial commit

---
+++ src/test/java/com/musinsa/coord/CoordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.coord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
